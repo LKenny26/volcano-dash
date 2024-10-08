1d93257c84b925ae7f7a6e76e508509cd39b439d
CPhuong24 <phuong24@up.edu>
2024-10-07 20:43:15 -0700

    Added Camera Movement

--- volcano-dash/player.gd
+++ volcano-dash/player.gd
@@ -1,10 +1,16 @@
 extends Node3D
 
+var sens = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+# Function to move camera around according to mouse movement
+func _input(event: InputEvent) -> void:
+		if event is InputEventMouseMotion:
+			rotate_y(deg_to_rad(-event.relative.x * sens))
+			$FIRST.rotate_x(deg_to_rad(-event.relative.y * sens))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
